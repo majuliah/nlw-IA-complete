8da87968b90777bda9e2c7b6966a75fd2637916b
majulha <majulialcarvalho@gmail.com>
2023-09-14 20:45:28 -0300

    implementando api

--- upload-ia-api/src/routes/get-all-prompts.ts
+++ upload-ia-api/src/routes/get-all-prompts.ts
@@ -1,7 +1,7 @@
 import { FastifyInstance } from "fastify";
 import { prisma } from "../lib/prisma";
 
-export function getAllPromptsRoute(app: FastifyInstance){
+export async function getAllPromptsRoute(app: FastifyInstance){
     app.get('/prompts', async () => {
         const prompts = await prisma.prompt.findMany();
         return prompts;
--- upload-ia-api/src/server.ts
+++ upload-ia-api/src/server.ts
@@ -1,10 +1,14 @@
 import { fastify } from "fastify";
 import { prisma } from "./lib/prisma";
 import { getAllPromptsRoute } from "./routes/get-all-prompts";
+import { uploadVideoRoute } from "./routes/upload-video";
+import { createTranscriptionRout } from "./routes/create-trasncription";
 
 const app = fastify();
 
 app.register(getAllPromptsRoute);
+app.register(uploadVideoRoute);
+app.register(createTranscriptionRout);
 
 app.listen({
     port: 3333,
